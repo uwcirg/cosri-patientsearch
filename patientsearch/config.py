0cc325a6581fee20d76d0f3852321c46703068c3
Ivan Cvitkovic <icvitkovic@gmail.com>
2020-06-18 13:29:21 -0700

    Attempt to decode JSON from env var if not JSON file

--- patientsearch/config.py
+++ patientsearch/config.py
@@ -1,11 +1,20 @@
+import json
 import os
+
+
+def decode_json_config(potential_json_string):
+    """Detect if given string is JSON file, or JSON string"""
+    if potential_json_string.endswith('.json'):
+        return potential_json_string
+    return json.loads(potential_json_string)
+
 
 SERVER_NAME = os.getenv("SERVER_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 EXTERNAL_FHIR_API = os.getenv("EXTERNAL_FHIR_API")
 MAP_API = os.getenv("MAP_API")
 
-OIDC_CLIENT_SECRETS = 'client_secrets.json'
+OIDC_CLIENT_SECRETS = decode_json_config(os.getenv("OIDC_CLIENT_SECRETS", "client_secrets.json"))
 OIDC_ID_TOKEN_COOKIE_SECURE = False
 OIDC_REQUIRE_VERIFIED_EMAIL = False
 OIDC_SCOPES = ['email', 'openid', 'roles']
