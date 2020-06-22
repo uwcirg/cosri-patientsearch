d6d280e8b256ae856186f719e283a6eb96bb8a81
Ivan Cvitkovic <ivanc@uw.edu>
2020-06-21 19:44:49 -0700

    Fixup var names

--- patientsearch/api.py
+++ patientsearch/api.py
@@ -167,12 +167,12 @@ def external_search(resource_type, methods=["GET"]):
     """
     token = validate_auth()
     external_search_bundle = external_request(token, resource_type, request.args)
-    patient_id = sync_bundle(token, external_search_bundle)
+    local_fhir_patient = sync_bundle(token, external_search_bundle)
 
     # TODO: handle multiple patient results
     if len(external_search_bundle['entry']) > 1:
         current_app.logger.warn('multiple patients returned from PDMP')
-    external_search_bundle['entry'][0].setdefault('id', patient_id)
+    external_search_bundle['entry'][0].setdefault('id', local_fhir_patient['id'])
 
     return jsonify(external_search_bundle)
 
