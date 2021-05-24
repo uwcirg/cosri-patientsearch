7ee93fa613890a46e9b6e2701c91c332b8029480
Paul Bugni <pbugni@u.washington.edu>
2021-05-24 12:19:46 -0700

    enable logout call for system in limbo (thinks it's authenticated, but it isn't)

--- patientsearch/api.py
+++ patientsearch/api.py
@@ -203,10 +203,18 @@ def external_search(resource_type, methods=["GET"]):
 @api_blueprint.route('/logout', methods=["GET"])
 def logout(methods=["GET"]):
     # TODO: is there a PHI safe 'id' for the user (in place of email)?
-    user_id = oidc.user_getfield('email')
-    current_app.logger.info(
-        "logout on request",
-        extra={'tags': ['logout'], 'user_id': user_id})
-    terminate_session()
+    try:
+        user_id = oidc.user_getfield('email')
+        current_app.logger.info(
+            "logout on request",
+            extra={'tags': ['logout'], 'user_id': user_id})
+        terminate_session()
+
+    except Exception as ex:
+        # Confirm the exception was simply a logout request
+        # from an unauthenticated user, or re-raise
+        if str(ex) != "User was not authenticated":
+            raise ex
+
     message = 'Logged out.  Return to <a href="/">COSRI Patient Search</a>'
     return make_response(message)
