d20579092657551ddc38fe65a069e9cdd2bf12c1
Paul Bugni <pbugni@u.washington.edu>
2021-07-07 14:51:00 -0700

    update exception text should the front-end refresh fail, as per https://www.pivotaltracker.com/story/show/178579291

--- patientsearch/api.py
+++ patientsearch/api.py
@@ -60,7 +60,10 @@ def validate_auth():
 
     if not oidc.validate_token(token):
         terminate_session()
-        raise Unauthorized("oidc token invalid")
+        raise Unauthorized(
+            "Your COSRI session timed out. "
+            "Please refresh your browser to enter your user name and password "
+            "to log back in.")
     return token
 
 
