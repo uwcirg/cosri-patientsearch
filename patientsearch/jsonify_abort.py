1380d75eb84c14f25bcb0b053c60e64ee0df08bf
Paul F Bugni <pbugni@uw.edu>
2021-09-02 15:36:34 -0700

    Make `status_code` a required, named variable as per code review

--- patientsearch/jsonify_abort.py
+++ patientsearch/jsonify_abort.py
@@ -1,9 +1,8 @@
 """Utility function to bring together flask `abort` and `jsonify`"""
 from flask import abort, jsonify, make_response
 
-def jsonify_abort(**kwargs):
+def jsonify_abort(status_code, **kwargs):
     """Takes any number of args, like jsonify, but raises like abort with correct content_type heading"""
-    status_code = kwargs.pop('status_code', 500)
     response = make_response(jsonify(kwargs))
     response.status_code = status_code
     abort(response)
