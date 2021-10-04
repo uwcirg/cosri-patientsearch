964534f7903817c844441457278a6413a14a2bd1
Amy Chen <clone@cesium.cirg.washington.edu>
2021-10-04 10:26:22 -0700

    fix default sort error display per feedback

--- patientsearch/src/js/components/PatientListTable.js
+++ patientsearch/src/js/components/PatientListTable.js
@@ -146,7 +146,7 @@ export default function PatientListTable(props) {
     {title: "Last Name", field: "last_name", filterPlaceholder: "Last Name", emptyValue: "--"},
     {title: "Birth Date", field: "dob", filterPlaceholder: "YYYY-MM-DD", emptyValue: "--"},
     /* the field for last accessed is patient.meta.lastupdated? */
-    {title: "Last Accessed", field: "lastUpdated", filtering: false, align: "center"}
+    {title: "Last Accessed", field: "lastUpdated", filtering: false, align: "center", defaultSort: "desc"}
   ];
   const errorStyle = {"display" : errorMessage? "block": "none"};
   const toTop = () => {
