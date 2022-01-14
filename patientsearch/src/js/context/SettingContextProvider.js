b53b1f08827066caa6ef3d502501c471d461c0a7
Amy Chen <clone@cesium.cirg.washington.edu>
2022-01-14 13:23:01 -0800

    add comment

--- patientsearch/src/js/context/SettingContextProvider.js
+++ patientsearch/src/js/context/SettingContextProvider.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState, useEffect, createContext } from "react";
 import {getSettings} from "../components/Utility";
 const SettingContext = React.createContext();
-
+/*
+ * context provider component that allows application settings to be accessible to its children component(s)
+ */
 export default function SettingContextProvider({children}) {
     const [appSettings, setAppSettings] = useState({});
     useEffect(() => {
@@ -13,6 +15,9 @@ export default function SettingContextProvider({children}) {
     }, []);
     return <SettingContext.Provider value={{appSettings}}>{children}</SettingContext.Provider>
 }
+/*
+ * helper function to access application setting context
+ */
 export function useSettingContext() {
     const context = useContext(SettingContext);
     if (context === undefined) {
