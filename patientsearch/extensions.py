3b47e9d226387021c86265a2506b3db120852ba6
pbugni <pbugni@u.washington.edu>
2022-06-07 15:43:09 -0700

    Remove common bookmark redirect from monitored filters (#137)
    * Users with a post-auth bookmark returning pre-auth generate a log message that is being picked up by the monitoring systems.
    Reduce the log level given the common case is not exceptional.
    
    * Black formatting.

--- patientsearch/extensions.py
+++ patientsearch/extensions.py
@@ -7,7 +7,9 @@ class OpenIDConnectRedirect(OpenIDConnect):
 
     def _oidc_error(self, message=None, code=None):
         message = f": {message}" if message is not None else ""
-        current_app.logger.error(f"Error{message}; redirecting to /home")
+        current_app.logger.warn(
+            f"{message}; redirecting to /home, presumably from post-auth bookmark"
+        )
         return redirect("/home")
 
 
