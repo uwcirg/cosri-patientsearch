1a4b3ef21830ea56d080378db4200705e343a1d6
Paul Bugni <pbugni@u.washington.edu>
2022-11-02 19:40:07 -0700

    formatting to keep black happy.

--- tests/test_columnconfig.py
+++ tests/test_columnconfig.py
@@ -18,7 +18,7 @@ def patient_bundle(datadir):
 
 
 def test_id_count(patient_bundle):
-    assert patient_bundle['total'] == 7
+    assert patient_bundle["total"] == 7
     expr = jmespath.compile("entry[?resource.resourceType=='Patient'].resource.id")
     results = expr.search(patient_bundle)
     assert len(results) == 7
@@ -33,17 +33,23 @@ def test_max_authored_by_patient(patient_bundle):
 
     for id in patient_ids:
         # from each patient ID, obtain the list of QuestionnaireResponse.authored values
-        expr = jmespath.compile(f"entry[?resource.subject.reference=='Patient/{id}'].resource.authored")
+        expr = jmespath.compile(
+            f"entry[?resource.subject.reference=='Patient/{id}'].resource.authored"
+        )
 
         # create a list of sortable datetime objects from authored date strings
-        auth_dates = [datetime.strptime(a, date_format) for a in expr.search(patient_bundle)]
+        auth_dates = [
+            datetime.strptime(a, date_format) for a in expr.search(patient_bundle)
+        ]
         auth_dates.sort()
 
         # retain string form for max found for patient, or empty string
-        results[id] = datetime.strftime(auth_dates[-1], date_format) if auth_dates else ""
+        results[id] = (
+            datetime.strftime(auth_dates[-1], date_format) if auth_dates else ""
+        )
 
     # Confirm a few known
     assert len(results) == 7
-    assert results['53'] == "2022-11-02"
-    assert results['test-patient-1'] == ""
-    assert results['test-patient-2'] == "2022-11-02"
+    assert results["53"] == "2022-11-02"
+    assert results["test-patient-1"] == ""
+    assert results["test-patient-2"] == "2022-11-02"
