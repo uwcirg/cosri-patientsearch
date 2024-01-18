f62ae676a369b093a901bc7f54a10a965fe420f0
Daniil <94884910+Filienko@users.noreply.github.com>
2024-01-18 09:20:10 -0800

    Formatting

--- patientsearch/api.py
+++ patientsearch/api.py
@@ -357,7 +357,7 @@ def update_resource_by_id(resource_type, resource_id):
                     "active": "true",
                 }
 
-                active_patient = HAPI_request(
+                duplicates = HAPI_request(
                     token=token,
                     method="GET",
                     resource_type=resource_type,
@@ -365,9 +365,9 @@ def update_resource_by_id(resource_type, resource_id):
                 )
 
                 # Raise a 500 error if active patients with the same phone number have been found
-                if active_patient["total"] > 0:
-                    first = active_patient["entry"][0]["resource"]["name"][0]["given"][0]
-                    last = active_patient["entry"][0]["resource"]["name"][0]["family"]
+                if duplicates["total"] > 0:
+                    first = duplicates["entry"][0]["resource"]["name"][0]["given"][0]
+                    last = duplicates["entry"][0]["resource"]["name"][0]["family"]
                     error_message = f"""The account can't be restored because
                         it's phone number, {telecom_value} is now used by another
                         account {first} {last}"""
